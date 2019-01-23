342d7f8721680627e6f954098ae608c317c5a9b6
Colin Dess <pcolind@gmail.com>
2019-01-23 09:30:12 -0800

    V 2.0
    Added UK tides and more robust data transfer from companion to device

--- common/utils.js
+++ common/utils.js
@@ -8,26 +8,3 @@ export function zeroPad(i) {
   }
   return i;
 }
-
-// calculate time until the next tide, and export the tide style
-export function countdown(globalTides, today) {
-  let tideChart = JSON.parse(globalTides);
-  for (var i = 0; i < tideChart.length; i ++) {
-    let offset = today.getTimezoneOffset()
-    let tempTime = tideChart[i]['t'].toString();
-    let localMinute = Math.floor(today.getTime()/60000);
-    let tideMinute = Math.floor((Date.parse(tempTime.split(' ').join('T'))+(60*offset*1000))/60000);
-    if (localMinute < tideMinute) {
-      let goodies = [tideMinute - localMinute, tideChart[i]['type']];
-      i = tideChart.length;
-    };
-  };
-  return goodies;
-};
-
-// set AM or PM
-export function hourMe(hours) {
-  let houring = 'AM';
-  if (hours >= 12) {houring = 'PM'};
-  return houring;
-};
